fd63aa3de004b2c59c3ac083a2dd72065de7152e
changfish <100700089+changfish@users.noreply.github.com>
2022-04-17 15:33:01 +0800

    The book thief
    language:c++
    test:2022/03/22
    practice:2022/4/17

---
+++ 12908.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main(){
+    int k,n;
+    cin>>k;
+    n=int((-1+sqrt(1+8*k))/2);
+    for(int i=n-10;i<=n+10;i++){
+    	int sum=(i)*(i+1)/2;
+    	if((sum>k)&&(i>0)){
+    		cout<<sum-k<<" "<<i;
+    		break;
+		}else{
+			if(i<=0)
+				n+=10;
+		}
+	}
+}
