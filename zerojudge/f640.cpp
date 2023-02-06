f0de6643625222b95391887b1908824a4a9868ee
changfish <100700089+changfish@users.noreply.github.com>
2023-02-06 15:56:30 +0800

    f640 函數運算式求值#遞迴#手刻atoi
    https://zerojudge.tw/ShowProblem?problemid=f640
    合成函數運用 ap325 P.18
    20230206才搞懂
    AC (7ms, 356KB)

--- zerojudge/f640.cpp
+++ zerojudge/f640.cpp
@@ -1,32 +1,49 @@
 #include <iostream>
-#include <cstdlib>
 using namespace std;
+#define ll long long 
 
-int m(){
-	int x,y,z;
-	char A[7];
-	cin>>A;
-	if(A[0]=='f'){
-		x=m();
-		return 2*x-3;
-	}else{
-		if(A[0]=='g'){
-			x=m();
-			y=m();
-			return 2*x+y-7;
-		}else{
-			if(A[0]=='h'){
-				x=m();
-				y=m();
-				z=m();
-				return 3*x-2*y+z;	
-			}else
-				return atoi(A);
+int atoi(string s){
+	int sum=0,bit=1;
+	if(s[0]=='-'){
+		for(int i=s.length()-1;i>=1;i--){
+			sum+=(s[i]-'0')*bit;
+			bit*=10;
 		}
+		return sum*(-1);
+	}else{
+		for(int i=s.length()-1;i>=0;i--){
+			sum+=(s[i]-'0')*bit;
+			bit*=10;
+		}
+		return sum;
 	}
 }
 
-int main()
-{
-	cout<<m();
+ll eval(){
+	int x,y,z;
+	string s;
+	cin>>s;
+	if(s[0]=='f'){
+		x=eval();
+		return 2*x-3;
+	}else{
+		if(s[0]=='g'){
+			x=eval();
+			y=eval();
+			return 2*x+y-7;
+		}else{
+			if(s[0]=='h'){
+				x=eval();
+				y=eval();
+				z=eval();
+				return 3*x-2*y+z;
+			}else{
+				return atoi(s);
+			}
+		}
+	}
 } 
+
+int main() {
+	cout<<eval();
+}
