7a3a1f32c1f746df71d94d4ce25b424955d69135
chang yu <100700089+changfish@users.noreply.github.com>
2023-04-02 15:33:54 +0800

    a993 10127 - Ones#mod
    https://zerojudge.tw/ShowProblem?problemid=a993
    如果a%b=m，則(c*a+k)%b=(c'*m+k)%b=(m+k)%b
    AC (64ms, 316KB)

--- zerojudge/a993.cpp
+++ zerojudge/a993.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
 
+
+int main(){
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
+	int bit=1,n,a;
+	while(cin>>n){
+		bit=1,a=1;
+		while(a%n){
+			a = 10*(a%n)+1;
+			bit++;
+		}
+		cout<<bit<<endl;
+	}
+}
